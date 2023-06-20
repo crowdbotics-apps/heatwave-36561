e9b4745c286fab5b6f990cb611fe9929a568b0d6
Blake Zweigle <blake@crowdbotics.com>
2023-06-20 11:58:25 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "heatwave_36561",
   "url": "https://heatwave_36561.botics.co",
   "api": "https://heatwave_36561.botics.co/api/v1",
-  "initialRoute": "Abouttheappscreen"
+  "initialRoute": "ScreenAI15"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
--- screens/ScreenAI15/index.js
+++ screens/ScreenAI15/index.js
@@ -1,9 +1,43 @@
-import React from 'react';
-import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { GLView } from "expo-gl";
+import { Renderer } from "expo-three";
+import { Scene, PerspectiveCamera, Mesh, BoxGeometry, MeshStandardMaterial, AmbientLight, DirectionalLight } from "three";
 
 const WelcomeScreen = () => {
+  const onContextCreate = async gl => {
+    const renderer = new Renderer({
+      gl
+    });
+    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+    const scene = new Scene();
+    const camera = new PerspectiveCamera(75, gl.drawingBufferWidth / gl.drawingBufferHeight, 0.1, 1000);
+    camera.position.z = 5;
+    const geometry = new BoxGeometry(1, 1, 1);
+    const material = new MeshStandardMaterial({
+      color: 0xff0000
+    });
+    const cube = new Mesh(geometry, material);
+    scene.add(cube);
+    const ambientLight = new AmbientLight(0xffffff, 0.8);
+    scene.add(ambientLight);
+    const directionalLight = new DirectionalLight(0xffffff, 0.8);
+    directionalLight.position.set(1, 1, 1);
+    scene.add(directionalLight);
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+      gl.endFrameEXP();
+    };
+
+    animate();
+  };
+
   return <View style={styles.container}>
-      <ImageBackground source={require('../assets/fireball.png')} style={styles.logo} />
+      <GLView style={styles.logo} onContextCreate={onContextCreate} />
       <Text style={styles.title}>Welcome to Heatwave</Text>
       <Text style={styles.subtitle}>The hottest app in town</Text>
     </View>;
@@ -12,8 +46,8 @@ const WelcomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     backgroundColor: "#b64141"
   },
   logo: {
@@ -23,13 +57,13 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 28,
-    fontWeight: 'bold',
-    color: '#fff',
+    fontWeight: "bold",
+    color: "#fff",
     marginBottom: 10
   },
   subtitle: {
     fontSize: 18,
-    color: '#fff'
+    color: "#fff"
   }
 });
 export default WelcomeScreen;
